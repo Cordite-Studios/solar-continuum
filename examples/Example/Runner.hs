c9f6d835cd48031fe2d6cbda7d8dcae654114ac5
Levi Schuck <levi.schuck@gmail.com>
2014-11-23 23:08:04 -0800

    Adding the worst runner ever.

--- examples/Example/Runner.hs
+++ examples/Example/Runner.hs
@@ -1,3 +1,25 @@
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Example.Runner where
 
--- hai
+import Control.Monad.Free
+
+import Solar.Continuum.Log
+import Solar.Continuum.LogF
+import Solar.Continuum.Types
+
+
+runLogIO :: Log a -> IO a
+runLogIO = iterM run where
+    run :: LogF (IO a) -> IO a
+    run (LogExists _ next) = next False
+    run (TellLogReadPosition _ next) = next 0
+    run (TellLogWritePosition _ next) = next $ Known 0
+    run (TellLogSize _ next) = next 0
+    run (CheckSumLog _ next) = next ""
+    run (ResetLogPosition _ next) = next
+    run (SkipForwardLog _ _ next) = next
+    run (DeleteLog _ next) = next
+    run (RenameLog _ _ next) = next LogNotFound
+    run (ReadFromLog _ next) = next $ Left "Worst runner ever."
+    run (AppendLogData _ _ next) = next
